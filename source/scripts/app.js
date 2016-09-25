688d7a2ec6f7586ef3f743d5e920985e4544a05c
CristhGunners <gunners_one@hotmail.com>
2016-09-25 12:21:23 -0300

    Add favicon & back to top button

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -34,6 +34,31 @@ $(document).on("ready", function(){
       windowPopup($(this).attr("href"), 500, 300);
   });
 
+  var offset = 300,
+  //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  offset_opacity = 1200,
+  //duration of the top scrolling animation (in ms)
+  scroll_top_duration = 700,
+  //grab the "back to top" link
+  $back_to_top = $('#top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+        $back_to_top.addClass('fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+
   function getColordiv(code) {
     return "<div class='palette__color'><div class='palette__color__circle' style='background-color:"+code+";' data-clipboard-text='"+code+"'></div><h3 class='palette__color__code'>"+code+"</h3></div>"
   }
