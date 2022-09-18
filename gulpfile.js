ff598cf59ceb53cd8bafa18a8f39ce08663f4c2f
CristhGunners <gunners_one@hotmail.com>
2022-09-18 01:26:14 -0300

    fix: Add default task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -33,71 +33,103 @@ var paths = {
   images: 'site/images',
   css: './site/css',
   js: './site/js',
-  html: './site'
+  html: './site',
 };
 
 // Jade Compile
-gulp.task('jade2html', function() {
-  gulp.src([paths.jadeEntry, paths.jadePages])
-  .pipe(jade({
-    pretty: false
-  }))
-  .pipe(reload({stream: true}))
-  .pipe(gulp.dest(paths.html));
+gulp.task('jade2html', function () {
+  gulp
+    .src([paths.jadeEntry, paths.jadePages])
+    .pipe(
+      jade({
+        pretty: false,
+      })
+    )
+    .pipe(reload({ stream: true }))
+    .pipe(gulp.dest(paths.html));
 });
 
 // Stylus Compile
 gulp.task('stylus2css', function () {
-  gulp.src([paths.stylusEntry])
-  .pipe(stylus({
-    use: [nib()],
-    import: ['nib'],
-    compress:true,
-    'include css': true,
-  }))
-  .pipe(reload({stream: true}))
-  .pipe(gulp.dest(paths.css));
+  gulp
+    .src([paths.stylusEntry])
+    .pipe(
+      stylus({
+        use: [nib()],
+        import: ['nib'],
+        compress: true,
+        'include css': true,
+      })
+    )
+    .pipe(reload({ stream: true }))
+    .pipe(gulp.dest(paths.css));
 });
 
 // Javascripts Concat + Compress
-gulp.task('scripts', function() {
-  return gulp.src([paths.jsEntry, paths.jsFiles])
-  .pipe(concat('app.js'))
-  .pipe(uglify())
-  .pipe(reload({stream: true}))
-  .pipe(gulp.dest(paths.js));
+gulp.task('scripts', function () {
+  return gulp
+    .src([paths.jsEntry, paths.jsFiles])
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(reload({ stream: true }))
+    .pipe(gulp.dest(paths.js));
 });
 
 // Minify Images
-gulp.task('minimages', function(){
-  gulp.src(paths.imagesSource)
-  .pipe(newer(paths.images))
-  .pipe(imagemin())
-  .pipe(gulp.dest(paths.images));
+gulp.task('minimages', function () {
+  gulp
+    .src(paths.imagesSource)
+    .pipe(newer(paths.images))
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images));
 });
 
 // Move Font & Icon Folder
-gulp.task('movefolders', [], function() {
+gulp.task('movefolders', [], function () {
   gulp.src(paths.fontsSource).pipe(gulp.dest(paths.fonts));
   gulp.src(paths.iconsSource).pipe(gulp.dest(paths.icons));
   gulp.src(paths.dataSource).pipe(gulp.dest(paths.data));
 });
 
 // Broser Sync
-gulp.task('browser-sync', [], function() {
+gulp.task('browser-sync', [], function () {
   browserSync({
     server: {
-      baseDir: './site'
+      baseDir: './site',
     },
-    open: false
+    open: false,
   });
 });
 
 // Watch Files
-gulp.task('serve', ['jade2html', 'stylus2css', 'scripts', 'minimages', 'movefolders', 'browser-sync'],function() {
-  gulp.watch([paths.jadeEntry,paths.jadePages, paths.jadeComponents], ['jade2html'], reload);
-  gulp.watch([paths.stylusEntry,paths.stylusComponents], ['stylus2css'], reload);
-  gulp.watch([paths.jsEntry,paths.jsFiles], ['scripts'], reload);
-  gulp.watch(paths.imagesSource, ['minimages']);
-  gulp.watch([paths.fontsSource, paths.iconsSource, paths.dataSource], ['movefolders']);
-});
+gulp.task(
+  'serve',
+  [
+    'jade2html',
+    'stylus2css',
+    'scripts',
+    'minimages',
+    'movefolders',
+    'browser-sync',
+  ],
+  function () {
+    gulp.watch(
+      [paths.jadeEntry, paths.jadePages, paths.jadeComponents],
+      ['jade2html'],
+      reload
+    );
+    gulp.watch(
+      [paths.stylusEntry, paths.stylusComponents],
+      ['stylus2css'],
+      reload
+    );
+    gulp.watch([paths.jsEntry, paths.jsFiles], ['scripts'], reload);
+    gulp.watch(paths.imagesSource, ['minimages']);
+    gulp.watch(
+      [paths.fontsSource, paths.iconsSource, paths.dataSource],
+      ['movefolders']
+    );
+  }
+);
+
+gulp.task('default', ['serve']);
